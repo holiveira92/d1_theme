9e8a20ad0719a013917a7a4b40480b79d73bdd32
Hiago Oliveira <hiagojardimdeoliveira@hotmail.com>
2019-10-23 22:14:16 -0300

    acerto em variavel errada

--- index.php
+++ index.php
@@ -31,13 +31,13 @@ global $wpdb;
 $d1_view_parser = new D1_View_Parser();
 //Obter todas as opções da página
 $GLOBALS["data"] = $d1_view_parser->get_data();
-$data = $GLOBALS["data"]["d1_plugin"];
+$data_home = $GLOBALS["data"]["d1_plugin"];
 get_header();
 
 ?>
 <head>
     <meta charset="utf-8">
-    <title><?php echo $data['d1_web_title'];?></title>
+    <title><?php echo $data_home['d1_web_title'];?></title>
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta content="Webflow" name="generator">
     <link href="<?php echo get_template_directory_uri().'/';?>css/normalize.css" rel="stylesheet" type="text/css">
@@ -48,7 +48,7 @@ get_header();
     <script src="<?php echo get_template_directory_uri().'/';?>js/home.js" type="text/javascript"></script>
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart" in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document)</script>
     <link href="<?php echo get_template_directory_uri().'/';?>icons/webclip.png" rel="apple-touch-icon">
-    <link href="<?php echo $data['d1_favico'];?>" rel="shortcut icon" type="image/x-icon">
+    <link href="<?php echo $data_home['d1_favico'];?>" rel="shortcut icon" type="image/x-icon">
 </head>
 
 <body>
@@ -58,14 +58,14 @@ get_header();
         <div class="mycontainer">
             <div class="home-hero-wrapper">
                 <div class="home-hero-left" data-ix="fade-in-on-load">
-                    <h1 class="white pad20"><?php echo insert_degrade($data['secao1_hero_title'],2);?></h1>
-                    <div class="white pad20"><?php echo $data['secao1_descricao_primaria'];?></div>
-                    <a href="<?php echo $data['secao1_conheca_um_minuto'];?>" class="btn-gradient w-button">CONHEÇA EM 1 MINUTO</a>
+                    <h1 class="white pad20"><?php echo insert_degrade($data_home['secao1_hero_title'],2);?></h1>
+                    <div class="white pad20"><?php echo $data_home['secao1_descricao_primaria'];?></div>
+                    <a href="<?php echo $data_home['secao1_conheca_um_minuto'];?>" class="btn-gradient w-button">CONHEÇA EM 1 MINUTO</a>
                 </div>
                 <div class="home-hero-right">
-                    <h3 class="white"><?php echo $data['secao1_hero_name'];?></h3>
-                    <h6 class="lightblue"><?php echo $data['secao1_hero_cargo'];?></h6>
-                    <div class="white pad20"><?php echo $data['secao1_hero_descricao'];?></div><img src="<?php echo $data['secao1_hero_company'];?>" alt="" class="home-hero-logo-partner"></div>
+                    <h3 class="white"><?php echo $data_home['secao1_hero_name'];?></h3>
+                    <h6 class="lightblue"><?php echo $data_home['secao1_hero_cargo'];?></h6>
+                    <div class="white pad20"><?php echo $data_home['secao1_hero_descricao'];?></div><img src="<?php echo $data_home['secao1_hero_company'];?>" alt="" class="home-hero-logo-partner"></div>
             </div>
         </div>
     </div>
@@ -73,7 +73,7 @@ get_header();
         <div>
             <div class="mycontainer">
                 <div class="section-title-2col">
-                    <h6 class="white pad20"><?php echo $data['secao1_descricao_secundaria'];?></h6>
+                    <h6 class="white pad20"><?php echo $data_home['secao1_descricao_secundaria'];?></h6>
                     <a href="#" class="link-text-white">VER CASES</a>
                 </div>
             </div>
@@ -87,7 +87,7 @@ get_header();
                 <?php
                 for($i=1;$i<=3;$i++):
                     $key_select = "secao2_select_card_cases" . $i;
-                    $query = "SELECT * FROM " . $wpdb->prefix . "d1_cases where id_card = '" . $data[$key_select] ."'";
+                    $query = "SELECT * FROM " . $wpdb->prefix . "d1_cases where id_card = '" . $data_home[$key_select] ."'";
                     $cards = json_decode(json_encode($wpdb->get_results($query)),true);
                     foreach($cards as $key=>$card){
                         
@@ -111,14 +111,14 @@ get_header();
     <div class="section-wrapper-large">
         <div class="mycontainer">
             <div class="section-title" data-ix="fade-in-on-scroll">
-                <h6 class="darkgrey pad20"><?php echo $data['secao2_empresas_title'];?></h6>
+                <h6 class="darkgrey pad20"><?php echo $data_home['secao2_empresas_title'];?></h6>
             </div>
             <div class="logo-clients-wrapper" data-ix="fade-in-on-scroll">
                 <?php
                 for($i=1;$i<=8;$i++):
                     $key_img = "secao2_img_empresa" . $i ;
                 ?>
-                    <div class="image-clientes-block"><img src="<?php echo $data[$key_img];?>" alt=""></div>
+                    <div class="image-clientes-block"><img src="<?php echo $data_home[$key_img];?>" alt=""></div>
                 <?php endfor; ?>
             </div>
         </div>
@@ -128,7 +128,7 @@ get_header();
     <div class="section-wrapper">
         <div class="mycontainer">
             <div class="section-title">
-                <h6 class="darkgrey pad20"><?php echo $data['secao4_section_title_part2'];?></h6>
+                <h6 class="darkgrey pad20"><?php echo $data_home['secao4_section_title_part2'];?></h6>
             </div>
             <div class="case-thumb-content-wrapper" data-ix="fade-in-on-scroll-2">
                 <?php
@@ -137,11 +137,11 @@ get_header();
                     $key_subtitle = "secao4_subtitle_card2_case" . $i ;
                     $key_link = "secao4_link_card2_case" . $i ;
                 ?>
-                    <a href="<?php echo $data[$key_link];?>" style='text-decoration:none;'>
+                    <a href="<?php echo $data_home[$key_link];?>" style='text-decoration:none;'>
                     <div class="desafio-thumb-content">
                         <div class="case-thumb-title">
-                            <h3 class="white"><?php echo $data[$key_title];?></h3>
-                            <div><?php echo $data[$key_subtitle];?></div>
+                            <h3 class="white"><?php echo $data_home[$key_title];?></h3>
+                            <div><?php echo $data_home[$key_subtitle];?></div>
                         </div>
                     </div>
                     </a>
@@ -154,9 +154,9 @@ get_header();
     <div class="section-wrapper-large black-bg-stripe">
         <div class="mycontainer">
             <div class="section-1col-wrapper" data-ix="fade-in-on-scroll-2">
-                <h1 class="white pad20"><?php echo $data['secao5_section_title'];?></h1>
-                <div class="white pad20"><?php echo $data['secao5_section_descricao'];?></div>
-                    <a href="<?php echo $data['secao5_link_calcular'];?>" class="btn-cx w-button">CALCULAR CX</a>
+                <h1 class="white pad20"><?php echo $data_home['secao5_section_title'];?></h1>
+                <div class="white pad20"><?php echo $data_home['secao5_section_descricao'];?></div>
+                    <a href="<?php echo $data_home['secao5_link_calcular'];?>" class="btn-cx w-button">CALCULAR CX</a>
                 </div>
         </div>
     </div>
@@ -166,7 +166,7 @@ get_header();
     <div class="tab-section-wrapper" data-ix="fade-in-on-scroll">
         <div class="mycontainer">
             <div class="tabs-section-title-2col">
-                <h6 class="pad20"><?php echo $data['secao6_title'];?></h6>
+                <h6 class="pad20"><?php echo $data_home['secao6_title'];?></h6>
                 <div class="link-text-arrow"><a href="#" class="link-text-black">VER CASES</a><img src="<?php echo get_template_directory_uri().'/';?>images/arrowlink-black.svg" alt="" class="arrowlink"></div>
             </div>
 
@@ -179,7 +179,7 @@ get_header();
                         $title_class = ($i == 1) ? 'w--current' : "";
                     ?>
                         <a data-w-tab="Tab <?php echo $i;?>" class="home-tab-link w-inline-block w-tab-link <?php echo $title_class;?>">
-                            <div><?php echo $data[$key_title];?></div>
+                            <div><?php echo $data_home[$key_title];?></div>
                         </a>
                     <?php endfor; ?>
                 </div>
@@ -204,7 +204,7 @@ get_header();
                                 ?>
 
                                 <a data-w-tab="Tab <?php echo $j;?>" class="home-tab-link w-inline-block w-tab-link <?php echo $subitem_tab_class;?>">
-                                    <div><?php echo $data[$subitem_title];?></div>
+                                    <div><?php echo $data_home[$subitem_title];?></div>
                                 </a>
                                 <?php endfor; ?>
                             </div>
@@ -224,11 +224,11 @@ get_header();
                                         <div class="line-gradient"></div>
                                         <div class="div-block-14">
                                             <p>
-                                                <?php echo $data[$subitem_descricao];?>
+                                                <?php echo $data_home[$subitem_descricao];?>
                                             </p>
-                                            <a href="<?php echo $data[$subitem_link];?>" class="text-link-blue"> Leia mais sobre </a>
+                                            <a href="<?php echo $data_home[$subitem_link];?>" class="text-link-blue"> Leia mais sobre </a>
                                         </div>
-                                        <div><img src="<?php echo $data[$subitem_image];?>" alt=""></div>
+                                        <div><img src="<?php echo $data_home[$subitem_image];?>" alt=""></div>
                                     </div>
                                 </div>
                             <?php endfor; ?>
@@ -250,12 +250,12 @@ get_header();
         <div class="mycontainer">
             <div class="section-2col-wrapper">
                 <div class="section-col-left" data-ix="fade-in-on-scroll">
-                    <h1 class="pad20"><?php echo $data['secao7_section_title'];?></h1>
+                    <h1 class="pad20"><?php echo $data_home['secao7_section_title'];?></h1>
                     <div class="pad20">
-                        <?php echo str_replace("\n",'<br />',$data['secao7_section_descricao']);?>
+                        <?php echo str_replace("\n",'<br />',$data_home['secao7_section_descricao']);?>
                     </div>
-                    <a target='_blank' href="<?php echo $data['secao7_link_cta_webinario'];?>" class="btn-black-home-play w-button">ASSISTA AO WEBINARIO</a></div>
-                <div class="section-colright"><img src="<?php echo $data['secao7_img'];?>" width="673"></div>
+                    <a target='_blank' href="<?php echo $data_home['secao7_link_cta_webinario'];?>" class="btn-black-home-play w-button">ASSISTA AO WEBINARIO</a></div>
+                <div class="section-colright"><img src="<?php echo $data_home['secao7_img'];?>" width="673"></div>
             </div>
         </div>
     </div>
